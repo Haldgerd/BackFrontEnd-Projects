19ad86c882cb1d8c221afe84c33a71988ed6d08c
Haldgerd <haldgerd.code@gmail.com>
2021-11-16 14:27:01 +0100

    "Added fron end javascript."

--- FrontendAPI-TMDB-server-project/app.js
+++ FrontendAPI-TMDB-server-project/app.js
@@ -1,58 +1,50 @@
+//IMPORTS
 import express from "express";
 import path from "path";
-
 //import fetch as it's not defined in node.js environment
 import fetch from "node-fetch";
+import { LOADIPHLPAPI } from "dns";
 
 
 //VARIABLES
 const PORT = 3000;
-
 const app = express();
-
 const IMAGE = "https://image.tmdb.org/t/p/w500/";
-
 const APIkey = "ad3ffbd0b196e926f7cccabfd2460f2a";
-
 const networkID = "213";
-
 const URL = "https://api.themoviedb.org/3/discover/tv?";
 
+
 //without path __dirname will not be recongized. Still exploring why this is so. Either connected to node version? Or it produced an error due to smthng else => https://stackoverflow.com/questions/8817423/why-is-dirname-not-defined-in-node-repl
-const __dirname = path.resolve();
-
-
+// const __dirname = path.resolve();
 
 // make static files avaliable. Static files are usualy stored in public dir.
 //anwser: https://stackoverflow.com/questions/40728554/resource-blocked-due-to-mime-type-mismatch-x-content-type-options-nosniff
 // answer with 10 upvotes did the trick.
-app.use(express.static(`${__dirname}/public/`));
+// app.use(express.static(`${__dirname}/public/`));
 
 
 
-// make a request to server, which will in turn respond
+// respond with series data
 app.get("/", (req, res) => {
+  // sending files
+  // res.sendFile(`${__dirname}/index.html`, (err) => {
 
-  res.sendFile(`${__dirname}/index.html`, (err) => {
+  //   if (err) {
+  //     console.log(`${err.name}: ${err.message}`);
+  //     res.end(err.messgage);
 
-    if (err) {
-      console.log(`${err.name}: ${err.message}`);
-      res.end(err.messgage);
-
-    }
-
-  });
-  
-  getSeries(URL)
-    .then(() => console.log("All OK"))
-    .catch((error) => console.log(error.message));
+  //   }
+  //}
+  const result = getSeries(URL);
 
 });
 
-//at search
+
+//at search address
 app.get("/search", (req, res) => {
 
-  res.send("This is a search page.");
+  res.json("This is a search page.");
 
 });
 
@@ -60,20 +52,28 @@ app.get("/search", (req, res) => {
 app.listen(PORT, () => console.log("LISTENING.."));
 
 
+
 //FUNCTIONS
+  //fetching data and sending it.
 const getSeries = async (url) => {
-  
+    
   const results = await fetch(url + `sort_by=popularity.desc&network_id=${networkID}&api_key=${APIkey}`);
 
   const data = await results.json();
 
   const dataResults = await data.results;
 
-  getTitleRatingDescription(dataResults);
+  const keywords = "with_keywords";
+
+
+  const finalData = getTitleRatingDescription(dataResults);
+
+  return finalData;
 
 }
 
 
+// retrieve selected data from series objects.
 const getTitleRatingDescription = (data) => {
   const series = [];
   
@@ -85,14 +85,23 @@ const getTitleRatingDescription = (data) => {
 
     const description = serie.overview;
 
+    const poster = serie.poster_path;
+
     series.push({
       title,
       rating,
-      description
+      description,
+      poster
     });
 
   });
 
-  console.log(series);
+  return series;
 
 }
+
+
+// const result = getSeries(URL);
+
+// result.then(() => console.log(result)).catch((error) => console.log(error.name, error,message));
+
--- FrontendAPI-TMDB-server-project/public/script.js
+++ FrontendAPI-TMDB-server-project/public/script.js
@@ -1,16 +1,75 @@
-// const URL = "https://api.themoviedb.org/3/movie/550?api_key=ad3ffbd0b196e926f7cccabfd2460f2a";
-// const IMAGE = "https://image.tmdb.org/t/p/w500/";
-// const APIkey = "ad3ffbd0b196e926f7cccabfd2460f2a";
+
+//VARIABLES
+const serverURL = "http://localhost:3000/";
+const searchData = "";
+const IMAGE = "https://image.tmdb.org/t/p/w500";
+const mainContainer = document.querySelector("main");
+const search = document.querySelector("input[type=text]");
 
 
-// async function getSeries(url) {
+//on reload page clear the value set in input field.
+search.value = "";
 
-//   const results = await fetch(url);
-//   const data = await results.json();
 
-//   console.log(data);
-// }
+// listening for sumbit => asking for data from backend using search Value as query string.
+window.addEventListener("submit", (e) => {
+  
+  e.preventDefault();
 
-// getSeries(URL)
-//   .then(() => console.log("All OK"))
-//   .catch((error) => console.log(error.message));
+  const searchValue = search.value; 
+
+  console.log(searchValue);
+  
+  // setTimeout(() => {
+  //   search.value = "";
+  // }, 2000)
+
+  //get data from other side. :) backend I mean.
+
+});
+
+
+// async function to call backend data retrieval
+const getData = async (url) => {
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  console.log(data);
+
+}
+
+
+getData(serverURL)
+
+  .then(() => console.log("Hello"))
+
+  .catch((error) => console.log(error.name, error.message));
+
+console.log("I'm really here!");
+
+
+
+//FUNCTIONS
+// display returned series data
+const displaySeries = (data) => {
+
+  data.forEach(element => {
+    
+    let container = document.createElement("div");
+
+    container.className = "series__container";
+
+    container.innerHTML = ` 
+      <img src="${IMAGE} + ${element.poster} alt="Series official poster" class="series__img" />
+      <div class="series__information">
+        <h3 class="series__title">${element.title}</h3>
+        <span class="rating">${element.rating}</span>
+      </div>
+      <div class="series__overview">${element.description}</div> `;
+
+    mainContainer.appendChild(container);
+    
+  });
+}
