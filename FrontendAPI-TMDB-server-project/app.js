40a22947d1d12c83f6ba49934f2442970abdd891
Haldgerd <haldgerd.code@gmail.com>
2021-11-16 12:33:54 +0100

    "Fetched data from TMDB - formating response data."

--- FrontendAPI-TMDB-server-project/app.js
+++ FrontendAPI-TMDB-server-project/app.js
@@ -1,13 +1,26 @@
 import express from "express";
 import path from "path";
 
+//import fetch as it's not defined in node.js environment
+import fetch from "node-fetch";
 
+
+//VARIABLES
 const PORT = 3000;
+
 const app = express();
 
+const IMAGE = "https://image.tmdb.org/t/p/w500/";
+
+const APIkey = "ad3ffbd0b196e926f7cccabfd2460f2a";
+
+const networkID = "213";
+
+const URL = "https://api.themoviedb.org/3/discover/tv?";
 
 //without path __dirname will not be recongized. Still exploring why this is so. Either connected to node version? Or it produced an error due to smthng else => https://stackoverflow.com/questions/8817423/why-is-dirname-not-defined-in-node-repl
 const __dirname = path.resolve();
+
 
 
 // make static files avaliable. Static files are usualy stored in public dir.
@@ -16,15 +29,70 @@ const __dirname = path.resolve();
 app.use(express.static(`${__dirname}/public/`));
 
 
+
 // make a request to server, which will in turn respond
 app.get("/", (req, res) => {
+
   res.sendFile(`${__dirname}/index.html`, (err) => {
+
     if (err) {
       console.log(`${err.name}: ${err.message}`);
       res.end(err.messgage);
+
     }
+
   });
+  
+  getSeries(URL)
+    .then(() => console.log("All OK"))
+    .catch((error) => console.log(error.message));
+
+});
+
+//at search
+app.get("/search", (req, res) => {
+
+  res.send("This is a search page.");
+
 });
 
 
 app.listen(PORT, () => console.log("LISTENING.."));
+
+
+//FUNCTIONS
+const getSeries = async (url) => {
+  
+  const results = await fetch(url + `sort_by=popularity.desc&network_id=${networkID}&api_key=${APIkey}`);
+
+  const data = await results.json();
+
+  const dataResults = await data.results;
+
+  getTitleRatingDescription(dataResults);
+
+}
+
+
+const getTitleRatingDescription = (data) => {
+  const series = [];
+  
+  data.map((serie) => {
+
+    const title = serie.name;
+
+    const rating = serie.vote_average;
+
+    const description = serie.overview;
+
+    series.push({
+      title,
+      rating,
+      description
+    });
+
+  });
+
+  console.log(series);
+
+}
---
+++ FrontendAPI-TMDB-server-project/public/script.js
@@ -0,0 +1,16 @@
+// const URL = "https://api.themoviedb.org/3/movie/550?api_key=ad3ffbd0b196e926f7cccabfd2460f2a";
+// const IMAGE = "https://image.tmdb.org/t/p/w500/";
+// const APIkey = "ad3ffbd0b196e926f7cccabfd2460f2a";
+
+
+// async function getSeries(url) {
+
+//   const results = await fetch(url);
+//   const data = await results.json();
+
+//   console.log(data);
+// }
+
+// getSeries(URL)
+//   .then(() => console.log("All OK"))
+//   .catch((error) => console.log(error.message));
