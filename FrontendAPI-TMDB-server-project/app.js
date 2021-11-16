02764df26462f616373561251979c6521d9036aa
Haldgerd <haldgerd.code@gmail.com>
2021-11-16 09:23:18 +0100

    "Added css file - for series cards,  added comments in app.js explaining __dirname variable. Added netflix logo."

--- FrontendAPI-TMDB-server-project/app.js
+++ FrontendAPI-TMDB-server-project/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import path from "path";
 
+
 const PORT = 3000;
 const app = express();
+
+
+//without path __dirname will not be recongized. Still exploring why this is so. Either connected to node version? Or it produced an error due to smthng else => https://stackoverflow.com/questions/8817423/why-is-dirname-not-defined-in-node-repl
 const __dirname = path.resolve();
 
 
@@ -10,6 +14,7 @@ const __dirname = path.resolve();
 //anwser: https://stackoverflow.com/questions/40728554/resource-blocked-due-to-mime-type-mismatch-x-content-type-options-nosniff
 // answer with 10 upvotes did the trick.
 app.use(express.static(`${__dirname}/public/`));
+
 
 // make a request to server, which will in turn respond
 app.get("/", (req, res) => {
